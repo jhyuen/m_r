3bc11ecbc6a5c58a00ee185b23480d124a86b201
Joseph Yuen <josephyuenpro@Josephs-MacBook-Pro.local>
2017-06-02 16:13:51 -0400

    I inserted the tutorial into the main storyboard

---
+++ MafiaRising/Tutorial18ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Tutorial18ViewController.swift
+//  MafiaRising
+//
+//  Created by Joseph Yuen on 6/2/17.
+//  Copyright © 2017 Joseph Yuen. All rights reserved.
+//
+
+import UIKit
+
+class Tutorial18ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
