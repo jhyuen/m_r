a06e0256bf764636bed117416f9225efe164faa1
Admin <admin@Admins-iMac.local>
2017-06-07 13:06:36 -0400

    Added transition functionality to end viewControllers

--- MafiaRising/Tutorial19AViewController.swift
+++ MafiaRising/Tutorial19AViewController.swift
@@ -11,25 +11,24 @@ import UIKit
 class Tutorial19AViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+            super.viewDidLoad()
+            let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(TutorialMainVC.swipeOccurred(_:)))
+            swipeLeft.direction = .left
+            view.addGestureRecognizer(swipeLeft)
+            // Do any additional setup after loading the view.
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        @IBAction func swipeOccurred(_ sender: Any) {
+            returnToOptions()
+        }
+        
+        
+        func returnToOptions() {
+            dismiss(animated: false, completion: nil)
+        }
 }
