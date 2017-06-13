c099f81a44659b3329281f328f0d8b22071555c2
Admin <admin@Admins-iMac.local>
2017-06-13 15:50:05 -0400

    Attempted to add camera view

--- MafiaRising/3CardRevealViewController.swift
+++ MafiaRising/3CardRevealViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
-class _CardRevealViewController: UIViewController {
+class _CardRevealViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var roleLbl: UILabel!
+    @IBOutlet weak var numberLbl: UILabel!
+    @IBOutlet weak var playerImage: UIImageView!
+    
+    var captureSession = AVCaptureSession()
+    var sessionOutput = AVCapturePhotoOutput()
+    var previewLayer = AVCaptureVideoPreviewLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let deviceSession = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInDualCamera,.builtInTelephotoCamera, .builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .unspecified)
+        
+        for device in (deviceSession?.devices)!{
+            if device.position == AVCaptureDevicePosition.front {
+                do {
+                    let input = try AVCaptureDeviceInput(device: device)
+                    if captureSession.canAddInput(input) {
+                        captureSession.addInput(input)
+                        
+                        if captureSession.canAddOutput(sessionOutput) {
+                            captureSession.addOutput(sessionOutput)
+                            
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            previewLayer.connection.videoOrientation = .portrait
+                            
+                            cameraView.layer.addSublayer(previewLayer)
+                            
+                            previewLayer.position = CGPoint(x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
+                            previewLayer.bounds = cameraView.frame
+                            
+                            captureSession.startRunning()
+                        }
+                    }
+                } catch let avError {
+                    print (avError)
+                }
+            }
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -31,14 +76,39 @@ class _CardRevealViewController: UIViewController {
         performSegue(withIdentifier: "CardsToInfo", sender: self)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Camera Button
+    @IBAction func takePhoto(_ sender: Any) {
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String : previewPixelType, kCVPixelBufferWidthKey as String : 160, kCVPixelBufferHeightKey as String : 160]
+        
+        settings.previewPhotoFormat = previewFormat
+        sessionOutput.capturePhoto(with: settings, delegate: self)
     }
-    */
-
+    
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        if let sampleBuffer = photoSampleBuffer, let previewBuffer = photoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+                    if error != nil {
+                        print("Error")
+                        return
+                    }
+            
+            cameraView.isHidden = true
+            
+            playerImage.image = UIImage(data: dataImage)
+            playerImage.isHidden = false
+            roleLbl.isHidden = false
+            numberLbl.isHidden = false
+            
+            captureSession.stopRunning()
+            previewLayer.removeFromSuperlayer()
+        }
+        
+    }
 }
