35b5e95b3441e9a9fa87dfaa2ea4a751170ce143
Joseph Yuen <josephyuenpro@Josephs-MacBook-Pro.local>
2017-06-20 14:10:01 -0400

    added options functionality and globa variables

--- MafiaRising/OptionsViewController.swift
+++ MafiaRising/OptionsViewController.swift
@@ -8,26 +8,66 @@
 
 import UIKit
 
+struct optionsParameters {
+    static var enableDirections: Bool = true
+    static var enableStory: Bool = true
+    static var soundEffectsVol: Float = 8
+    static var musicVol: Float = 8
+}
+
 class OptionsViewController: UIViewController {
 
+    @IBOutlet weak var soundEffectsSlider: UISlider!
+    @IBOutlet weak var musicSlider: UISlider!
+    @IBOutlet weak var directionsBtn: CheckBox!
+    @IBOutlet weak var storyBtn: CheckBox!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        soundEffectsSlider.value = optionsParameters.soundEffectsVol
+        musicSlider.value = optionsParameters.musicVol
+        directionsBtn.isChecked = optionsParameters.enableDirections
+        storyBtn.isChecked = optionsParameters.enableStory
+    }
+    
     // Back Button
     @IBAction func backBtnPressed(_ sender: Any) {
+        
+        optionsParameters.enableDirections = directionsBtn.isChecked
+        optionsParameters.enableStory = storyBtn.isChecked
+        
+        // test if options were updated
+        print(optionsParameters.enableDirections)
+        print(optionsParameters.enableStory)
+        print(optionsParameters.soundEffectsVol)
+        print(optionsParameters.musicVol)
+        
         dismiss(animated: true, completion: nil)
     }
     
     // Info Button
     @IBAction func goToOptionInfo(_ sender: Any) {
         performSegue(withIdentifier: "OptionToInfo", sender: self)
+    }
+    
+    // Sound Effects Slider Changed
+    @IBAction func soundEffectsSlider(_ sender: UISlider) {
+        optionsParameters.soundEffectsVol = sender.value
+    }
+    
+    // Music Slider Changed
+    @IBAction func musicSlider(_ sender: UISlider) {
+        optionsParameters.musicVol = sender.value
     }
 }
