97ab6c19fe4f26ccfb1381183840358565d322dc
Joseph Yuen <josephyuenpro@Josephs-MacBook-Pro.local>
2017-06-20 20:14:16 -0400

    redesigned randomized sound functions for easier expansion

--- MafiaRising/7ChooseViewController.swift
+++ MafiaRising/7ChooseViewController.swift
@@ -18,7 +18,9 @@ class _ChooseViewController: UIViewController {
     // transfer array
     var masterPlayerArray: Array<Player> = []
     
-    var soundEffectsArray: Array<SoundEffect> = []
+    // !!! maybe make another sound effect array for tribunal sounds, not just night phase !!!
+    var nightSoundEffectsArray: Array<SoundEffect> = []
+    var tribunalSoundEffectsArray: Array<SoundEffect> = []
     
     // Sound Effect Button Sounds
     var sE1: String = "se1"
@@ -35,63 +37,30 @@ class _ChooseViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-
-        /* TAKE OFF COMMENTS ONCE SOUND EFFECTS ARE INSERTED INTO ARRAY
-         
-        // Generate tag array
-        var soundEffectsIndex = [Int](repeating: 0, count: soundEffectsArray.count)
         
-        // Loop through number of sound effect buttons
-        for soundEffectNum in 1...4 {
-            
-            var nextIndex: Int
-            
-            repeat {
-                nextIndex = Int(arc4random_uniform(UInt32(soundEffectsArray.count)))
-            } while soundEffectsIndex[nextIndex] == 1
-            
-            // Set sound effect button with random sound and corresponding picture
-            switch soundEffectNum {
-                
-            case 1:
-                // Tag Index
-                soundEffectsIndex[nextIndex] = 1
-                // Picture
-                soundEffectBtn1.setBackgroundImage(UIImage(named: soundEffectsArray[nextIndex].picture), for: UIControlState.normal)
-                // Sound
-                sE1 = soundEffectsArray[nextIndex].effect
-            case 2:
-                // Tag Index
-                soundEffectsIndex[nextIndex] = 1
-                // Picture
-                soundEffectBtn2.setBackgroundImage(UIImage(named: soundEffectsArray[nextIndex].picture), for: UIControlState.normal)
-                // Sound
-                sE2 = soundEffectsArray[nextIndex].effect
-            case 3:
-                // Tag Index
-                soundEffectsIndex[nextIndex] = 1
-                // Picture
-                soundEffectBtn3.setBackgroundImage(UIImage(named: soundEffectsArray[nextIndex].picture), for: UIControlState.normal)
-                // Sound
-                sE3 = soundEffectsArray[nextIndex].effect
-            case 4:
-                // Tag Index
-                soundEffectsIndex[nextIndex] = 1
-                // Picture
-                soundEffectBtn4.setBackgroundImage(UIImage(named: soundEffectsArray[nextIndex].picture), for: UIControlState.normal)
-                // Sound
-                sE4 = soundEffectsArray[nextIndex].effect
-            default:
-                print("You have found a bug!!! Congratulations!!!")
-            }
-            
-        } 
-         */
+        /*
+        if part == 2 {
+            updateSounds(arrayType: nightSoundEffectsArray)
+        } else {
+            updateSounds(arrayType: tribunalSoundEffectsArray)
+        }
+        */
+        
     }
     
     // Pause Button
     @IBAction func pauseBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: "ChooseToPause", sender: self)
+    }
+    
+    // Repeat Button
+    @IBAction func repeatDirections(_ sender: Any) {
+        
+    }
+    
+    // Proceed Button
+    @IBAction func goToNextScreen(_ sender: Any) {
+        
     }
     
     // Sound Effect Buttons
@@ -113,6 +82,58 @@ class _ChooseViewController: UIViewController {
         print("\(sE4)")
     }
     
+    func updateSounds(arrayType: Array<SoundEffect>) {
+        
+         // Generate tag array
+         var soundEffectsIndex = [Int](repeating: 0, count: arrayType.count)
+         
+         // Loop through number of sound effect buttons
+         for soundEffectNum in 1...4 {
+         
+            var nextIndex: Int
+         
+            repeat {
+                nextIndex = Int(arc4random_uniform(UInt32(arrayType.count)))
+            } while soundEffectsIndex[nextIndex] == 1
+         
+            // Set sound effect button with random sound and corresponding picture
+            switch soundEffectNum {
+         
+            case 1:
+                // Tag Index
+                soundEffectsIndex[nextIndex] = 1
+                // Picture
+                soundEffectBtn1.setBackgroundImage(UIImage(named: arrayType[nextIndex].picture), for: UIControlState.normal)
+                // Sound
+                sE1 = arrayType[nextIndex].effect
+                
+            case 2:
+                // Tag Index
+                soundEffectsIndex[nextIndex] = 1
+                // Picture
+                soundEffectBtn2.setBackgroundImage(UIImage(named: arrayType[nextIndex].picture), for: UIControlState.normal)
+                // Sound
+                sE2 = arrayType[nextIndex].effect
+            case 3:
+                // Tag Index
+                soundEffectsIndex[nextIndex] = 1
+                // Picture
+                soundEffectBtn3.setBackgroundImage(UIImage(named: arrayType[nextIndex].picture), for: UIControlState.normal)
+                // Sound
+                sE3 = arrayType[nextIndex].effect
+            case 4:
+                // Tag Index
+                soundEffectsIndex[nextIndex] = 1
+                // Picture
+                soundEffectBtn4.setBackgroundImage(UIImage(named: arrayType[nextIndex].picture), for: UIControlState.normal)
+                // Sound
+                sE4 = arrayType[nextIndex].effect
+            default:
+                print("You have found a bug!!! Congratulations!!!")
+            }
+         
+        }
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ChooseToDay" {
